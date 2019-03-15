4ed5054d090ad089c491cbbe3df1e064034f2a2e
Adam Sales <asales@utexas.edu>
2019-03-15 12:12:46 -0500

    plot groups

--- lca.r
+++ lca.r
@@ -5,7 +5,7 @@ library(ggplot2)
 library(dplyr)
 library(poLCA)
 
-load('data/clusteringData1yr.RData')
+load('clusteringData1yr.RData')
 
 sdat2 <- subset(sdat,agep<41 & agep>=25 & !is.na(hincp)& esr==6 )#& inSchool==0)
 
@@ -59,6 +59,20 @@ lca5 <- poLCA(cbind(ageCat,hincCat,married,native,selfCare,blind,indepLiving,amb
 
 lca6 <- poLCA(cbind(ageCat,hincCat,married,native,selfCare,blind,indepLiving,ambulatory,cognitive,hupac,attain,white,sex,inSchool)~1,nclass=6,nrep=100,na.rm=FALSE,verbose=FALSE,calc.se=TRUE,data=lcadat)
 
+###  put them in the order I originally sent to Carrie Lou:
+adjp <- as.vector(crossprod(lca6$posterior,sdat2$pwgtp/sum(sdat2$pwgtp)))
+intendedP <- c(0.2346, 0.1177, 0.0773,0.3439, 0.1146, 0.1120)
+neword <- sapply(intendedP,function(p) which.min(abs(round(adjp,4)-p)))
+
+attempts <- lca6$attempts
+pstart <- lca6$probs.start
+pstart <- poLCA.reorder(pstart,neword)
+lca6 <- poLCA(#formula(lca6)
+    cbind(ageCat,hincCat,married,native,selfCare,blind,indepLiving,ambulatory,cognitive,hupac,attain,white,sex,inSchool)~1
+   ,lcadat, nclass=6,na.rm=FALSE,probs.start=pstart,verbose=FALSE,calc.se=TRUE)
+lca6$attempts <- attempts
+
+
 probsBig <- lca6$probs
 ad <- NULL
 oth <- sdat2[,'otherDiss']
@@ -105,12 +119,15 @@ lca6$probs <- lca6$probs[c(1:4,15,5:14)]
 lca6$probs <- nameCat(lca6$probs) #probsAll <- nameCat(probsAll)
 
 lca6$probs.se$other <- matrix(0,nrow(ad),ncol(ad))
-lca6$probs.se <- lca6$probs.se[c(1:4,14,5:14)]
+lca6$probs.se <- lca6$probs.se[c(1:4,15,5:14)]
 
-itemNames <- c('Age','Income\nQuartile','Marital\nStatus','Immig.','Deaf-\nDisabled','Self\n-Care','Vision','Indep.\nLiving','Amb.','Cog','Kids at\nHome','Ed.\nAttain.','Race','Sex','In\nSchool')
+
+itemNames <- c('Age','Income\nQuartile','Marital\nStatus','Immig.','Deafdisabled','Self\n-Care','Vision','Indep.\nLiving','Amb.','Cog','Kids at\nHome','Ed.\nAttain.','Race','Sex','In\nSchool')
 
 porig <- lca6$P
 lca6$P <- crossprod(lca6$posterior,sdat2$pwgtp/sum(sdat2$pwgtp))#/nrow(sdat2)
+
+save(lca6,file='lca6.RData')
 
 lcadat$other <- ifelse(sdat2$otherDiss, 'Yes','No')
 makeplot2(lca6,NULL,itemNames, calc='other', include.margin=TRUE,data=lcadat)
@@ -120,33 +137,100 @@ ggsave('pooledCluster6.png',height=6.5,width=9,units='in')
 probs <- lca6$probs
 for(nn in names(probs)) probs[[nn]] <- rbind(probs[[nn]],Overall=margProb(lcadat[[nn]]))
 
+probs.se <- lca6$probs.se
+for(nn in names(probs.se)){
+    probs.se[[nn]] <- rbind(probs.se[[nn]],Overall=0)
+    dimnames(probs.se[[nn]]) <- dimnames(probs[[nn]])
+}
+
+
+keepColName <- function(x,cc=1){
+    cnames <- colnames(x)
+    out <- x[,cc]
+    attr(out,"category") <- cnames[cc]
+    out
+}
+
+pdatFun <- function(varbs,se,vnames=NULL){
+    ppp <- if(se) probs.se else probs
+    pdat <- ppp[varbs]
+    for(i in 1:length(pdat))
+        if(ncol(pdat[[i]])==2){
+            print(paste('For variable',varbs[i],'keeping category',colnames(pdat[[i]])[1]))
+            pdat[[i]] <- pdat[[i]][,1]
+        }
+    pdat <- as.data.frame(pdat)
+    pdat$class <- gsub(': ','',simpleCap(rownames(ppp[[varbs[1]]])),fixed=TRUE)
+    pdat <- gather(pdat,'var','x',-class)
+    names(pdat)[names(pdat)=='x'] <- ifelse(se,'se','probability')
+    if(!is.null(vnames)){
+        for(i in 1:length(varbs)) pdat$var[pdat$var==varbs[i]] <- vnames[i]
+        pdat$var <- factor(pdat$var,levels=vnames)
+    } else pdat$var <- factor(pdat$var,levels=varbs)
+    pdat
+}
+
+plotGroup <- function(varbs,vnames=NULL,calc=NULL){
+    probDat <- pdatFun(varbs,se=FALSE,vnames=vnames)
+    seDat <- pdatFun(varbs,se=TRUE,vnames=vnames)
+    pdat <- merge(probDat,seDat)
+    pdat <- within(pdat,{
+                         ebmin=rowMax(cbind(probability-2*se,0))
+                         ebmax=rowMin(cbind(probability+2*se,1))
+                     }
+                   )
+    pdat$overall <- pdat$class=='Overall'
+    if(!is.null(calc)) pdat$overall[pdat$var%in%calc] <- TRUE
+
+    ggplot(pdat,aes(var,probability,fill=overall))+geom_col(position='dodge')+
+        facet_wrap(~class,nrow=1)+
+        theme(axis.text.x = element_text(angle = -90, hjust = 0,vjust=.5),
+                  legend.position='none',text=element_text(size=12))+
+        xlab(NULL)+
+        scale_y_continuous(name=NULL,labels=scales::percent,limits=c(0,1))+
+        geom_errorbar(aes(ymin=ebmin,ymax=ebmax),width=0)
+}
+
 ## plot disabilities
-disDat <- sapply(c('selfCare','blind','indepLiving','ambulatory','cognitive','other'),
-  function(x) probs[[x]][,1])
+plotGroup(c('other','selfCare','blind','indepLiving','ambulatory','cognitive')
+          vnames=c(other='Deafdisabled',selfCare='Self Care',blind='Blind',indepLiving='Independent Living',ambulatory='Ambulatory',cognitive='Cognitive'),
+          calc='Deafdisabled')
+ggsave('disability.png',width=6.5,height=3)
+
+
+
+disDat <- sapply(
+  function(x) round(probs[[x]][,1],5))
 disDat <- as.data.frame(disDat)
 disDat$class <- c(paste('Class',1:6),'Overall')
 
-library(tidyr)
 disDat <- gather(disDat, 'disability','probability',1:6)
+
+seDat <- sapply(c('selfCare','blind','indepLiving','ambulatory','cognitive','other'),
+  function(x) round(probs.se[[x]][,1],5))
+seDat <- as.data.frame(seDat)
+seDat$class <- c(paste('Class',1:6),'Overall')
+
+disDat <- gather(disDat, 'disability','probability',1:6)
+
 
 disDat$disability <- c(selfCare='Self Care',blind='Blind',indepLiving='Independent Living',ambulatory='Ambulatory',cognitive='Cognitive',other='Deafdisabled')[disDat$disability]
 
 disDat$disability <- factor(disDat$disability)
 disDat$disability <- relevel(disDat$disability,ref='Deafdisabled')
-disDat$overall <- disDat$disability=='Deafdisabled'|disDat$class=='Overall'
 
-ggplot(disDat,aes(disability,probability,fill=overall))+geom_col(position='dodge')+facet_wrap(~class,nrow=1)+theme(axis.text.x = element_text(angle = -90, hjust = 0,vjust=.5),legend.position='none',text=element_text(size=12))+xlab(NULL)
-ggsave('disability.png',width=6.5,height=3)
+
+ggplot(disDat,aes(disability,probability,fill=overall))+geom_col(position='dodge')+facet_wrap(~class,nrow=1)+theme(axis.text.x = element_text(angle = -90, hjust = 0,vjust=.5),legend.position='none',text=element_text(size=12))+xlab(NULL)+scale_y_continuous(name=NULL,labels=scales::percent,limits=c(0,1))
+
 
 ## plot demographics
 demDat <- sapply(c('native','white','sex'),function(x) probs[[x]][,1])
 demDat <- as.data.frame(demDat)
 demDat$class <- c(paste('Class',1:6),'Overall')
 
-library(tidyr)
 demDat <- gather(demDat, 'demographic','probability',1:3)
 
-demDat$demographic <- c(native='Native Born',white='White',sex='Female')
+demDat$demographic <- c(native='Native Born',white='White',sex='Female')[as.character(demDat$demographic)]
 
 demDat$demographic <- factor(demDat$demographic)
 demDat$overall <- demDat$class=='Overall'
@@ -172,13 +256,12 @@ ggplot(famDat,aes(fam,probability,fill=overall))+geom_col(position='dodge')+face
 ggsave('family.png',width=6.5,height=3)
 
 #### education
-edDat <- with(probs,cbind(attain,inSchool))
+edDat <- with(probs,cbind(attain,Enrolled=inSchool[,'Yes']))
 
 edDat <- as.data.frame(edDat)
 edDat$class <- c(paste('Class',1:6),'Overall')
 
-edDat <- gather(edDat, 'ed','probability',1:5)
-edDat$ed[edDat$ed=='Yes'] <- 'Enrolled'
+edDat <- gather(edDat, 'ed','probability',-class)
 
 edDat$ed <- factor(edDat$ed,levels=unique(edDat$ed))
 edDat$overall <- edDat$class=='Overall'
@@ -192,7 +275,7 @@ ageDat <- probs$age
 ageDat <- as.data.frame(ageDat)
 ageDat$class <- c(paste('Class',1:6),'Overall')
 
-ageDat <- gather(ageDat, 'age','probability',1:3)
+ageDat <- gather(ageDat, 'age','probability',-class)
 
 ageDat$age <- factor(ageDat$age,levels=unique(ageDat$age))
 ageDat$overall <- ageDat$class=='Overall'
@@ -206,9 +289,10 @@ incomeDat <- probs$hincCat
 incomeDat <- as.data.frame(incomeDat)
 incomeDat$class <- c(paste('Class',1:6),'Overall')
 
-incomeDat <- gather(incomeDat, 'income','probability',1:4)
+incomeDat <- gather(incomeDat, 'income','probability',-class)
 
 incomeDat$income <- factor(incomeDat$income,levels=unique(incomeDat$income))
+
 incomeDat$overall <- incomeDat$class=='Overall'
 
 ggplot(incomeDat,aes(income,probability,fill=overall))+geom_col(position='dodge')+facet_wrap(~class,nrow=1)+theme(axis.text.x = element_text(angle = -90, hjust = 0,vjust=.5),legend.position='none',text=element_text(size=12),plot.margin = unit(c(1,1,2,1), "lines"))+xlab(NULL)+scale_y_continuous(name=NULL,labels=scales::percent,limits=c(0,1))+ggtitle('Income')
@@ -217,47 +301,47 @@ ggsave('income.png',width=6.5,height=3)
 
 
 
-mean(rowMax(lca5$posterior)>0.8)
-lcasD <- lapply(1:10,function(k) poLCA(cbind(ageCat,hincCat,married,native,hupac,attain,white,sex)~1,nclass=k,nrep=20,na.rm=FALSE,verbose=FALSE,calc.se=FALSE,data=lcadat[!sdat2$otherDiss,]))
-sapply(lcasD,function(x) c(aic=x$aic,bic=x$bic))
+## mean(rowMax(lca5$posterior)>0.8)
+## lcasD <- lapply(1:10,function(k) poLCA(cbind(ageCat,hincCat,married,native,hupac,attain,white,sex)~1,nclass=k,nrep=20,na.rm=FALSE,verbose=FALSE,calc.se=FALSE,data=lcadat[!sdat2$otherDiss,]))
+## sapply(lcasD,function(x) c(aic=x$aic,bic=x$bic))
 
-odDat <- lcadat[sdat2$otherDiss,]
-odDat$cognitive[odDat$selfCare==2 & odDat$blind==2 & odDat$indepLiving==2 & odDat$ambulatory==2] <- NA
-lcasOD <- lapply(1:10,function(k) poLCA(cbind(ageCat,hincCat,married,native,selfCare,blind,indepLiving,ambulatory,cognitive,hupac,attain,white,sex)~1,nclass=k,nrep=20,na.rm=FALSE,verbose=FALSE,calc.se=FALSE,data=odDat))
-sapply(lcasOD,function(x) c(aic=x$aic,bic=x$bic))
+## odDat <- lcadat[sdat2$otherDiss,]
+## odDat$cognitive[odDat$selfCare==2 & odDat$blind==2 & odDat$indepLiving==2 & odDat$ambulatory==2] <- NA
+## lcasOD <- lapply(1:10,function(k) poLCA(cbind(ageCat,hincCat,married,native,selfCare,blind,indepLiving,ambulatory,cognitive,hupac,attain,white,sex)~1,nclass=k,nrep=20,na.rm=FALSE,verbose=FALSE,calc.se=FALSE,data=odDat))
+## sapply(lcasOD,function(x) c(aic=x$aic,bic=x$bic))
 
-margProb <- function(x) as.vector(table(x))/length(x)
+## margProb <- function(x) as.vector(table(x))/length(x)
 
-probsD <- nameCat(lcasD[[3]]$probs)
-makeplot2(probsD,itemNames=itemNames[c(1:4,11:14)],P=c(lcasD[[3]]$P,1))
-ggsave('lcaDeaf.pdf',height=6.5,width=9,units='in')
+## probsD <- nameCat(lcasD[[3]]$probs)
+## makeplot2(probsD,itemNames=itemNames[c(1:4,11:14)],P=c(lcasD[[3]]$P,1))
+## ggsave('lcaDeaf.pdf',height=6.5,width=9,units='in')
 
-probsOD <- lcasOD[[4]]$probs
-cog <- NULL
-cogDiff <- 2-lcadat[sdat2$otherDiss,'cognitive']
-numDis <- NULL
-nd <- with(lcadat[sdat2$otherDiss,], (selfCare==1) + (blind==1) +(indepLiving==1) +(ambulatory==1) +(cognitive==1))
-for(cc in 1:4){
-    post <- lcasOD[[4]]$posterior[,cc]
-    p <- sum(cogDiff*post)/sum(post)
-    cog <- rbind(cog,c(p,1-p))
-    ps <- sapply(1:5,function(n) sum((nd==n)*post)/sum(post))
-    numDis <- rbind(numDis,ps)
-}
+## probsOD <- lcasOD[[4]]$probs
+## cog <- NULL
+## cogDiff <- 2-lcadat[sdat2$otherDiss,'cognitive']
+## numDis <- NULL
+## nd <- with(lcadat[sdat2$otherDiss,], (selfCare==1) + (blind==1) +(indepLiving==1) +(ambulatory==1) +(cognitive==1))
+## for(cc in 1:4){
+##     post <- lcasOD[[4]]$posterior[,cc]
+##     p <- sum(cogDiff*post)/sum(post)
+##     cog <- rbind(cog,c(p,1-p))
+##     ps <- sapply(1:5,function(n) sum((nd==n)*post)/sum(post))
+##     numDis <- rbind(numDis,ps)
+## }
 
-probsOD$cognitive <- cog
-probsOD <- nameCat(probsOD)
+## probsOD$cognitive <- cog
+## probsOD <- nameCat(probsOD)
 
-colnames(numDis) <- 1:5
-numDis <- rbind(numDis,margProb(nd))
-probsOD$other <- numDis
-probsOD <- probsOD[names(probsAll)]
+## colnames(numDis) <- 1:5
+## numDis <- rbind(numDis,margProb(nd))
+## probsOD$other <- numDis
+## probsOD <- probsOD[names(probsAll)]
 
-probsOD <- lapply(names(probsOD),function(x) rbind(probsOD[[x]],margProb(lcadat[sdat2$otherDiss,x])))
+## probsOD <- lapply(names(probsOD),function(x) rbind(probsOD[[x]],margProb(lcadat[sdat2$otherDiss,x])))
 
-itemNames2 <- itemNames
-itemNames2[5] <- '# of\nDisabilities'
-makeplot2(probsOD, itemNames=itemNames2,P=c(lcasOD[[4]]$P,1))
-ggsave('lcaDeafDisabled.pdf',height=6.5,width=9,units='in')
+## itemNames2 <- itemNames
+## itemNames2[5] <- '# of\nDisabilities'
+## makeplot2(probsOD, itemNames=itemNames2,P=c(lcasOD[[4]]$P,1))
+## ggsave('lcaDeafDisabled.pdf',height=6.5,width=9,units='in')
 
 
